68e4446851c868f1ce54c858b15e39b70b23af00
adria mouzinho <adrianmouzinhopro@gmail.com>
2022-08-04 10:57:54 -0300

    first commit

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "dd5b3ad183cf26",
+    pass: "0c3f0424b03073"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Adrian Mouzinho <adrianmouzinho@gmail.com>',
+      subject,
+      html: body,
+    })
+  };
+}
